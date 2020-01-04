6afa727d267b4e92bf4cb7ea1b47de37520efaea
Pranav Kumar <pmkumar@andrew.cmu.edu>
2020-01-03 21:04:26 -0600

    Initial commit

---
+++ gdoc.py
@@ -0,0 +1,260 @@
+# Google Docs Interface for use in gdocsfs
+# Author: Pranav Kumar <pmkumar@cmu.edu>
+# 
+# Base64 encoding is used for content - Docs doesn't play well with escape chars
+#
+# We need both the Docs and Drive APIs - Docs for creating/editing files and
+# Drive for deleting them. As a result, we need two credential files and
+# service objects.
+
+
+from __future__ import print_function
+import base64
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the pickled token files
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+# Credentials for docs and drive
+CRED_DOCS = 'cred_docs.pickle'
+CRED_DRIVE = 'cred_drive.pickle'
+
+# Tokens for docs and drive
+TOKEN_DOCS = 'token_docs.pickle'
+TOKEN_DRIVE = 'token_drive.pickle'
+
+
+# The ID of a sample document called Blank
+DOCUMENT_ID = '1dRvSqJzTekzn_rY__8cnQ7B6w8QNnz94pmnsH8Ml3Iw'
+
+# The service objects for Docs and Drive
+SERVICE = None
+DRIVE_SERVICE = None
+
+# Docs is 1-indexed... have to add 1 to all write indices
+WRITE_OFFSET = 1
+
+###############
+### HELPERS ###
+###############
+
+# string -> base64 encoding
+def encb64(string):
+    encoded_bytes = base64.b64encode(string.encode('utf-8'))
+    b64_str = str(encoded_bytes, 'utf-8')
+    return b64_str
+
+# base64 encoding -> string
+def decb64(b64):
+    decoded_bytes = base64.b64decode(b64)
+    decoded_str = str(decoded_bytes, 'utf-8')
+    return decoded_str
+
+######################
+### INITIALIZATION ###
+######################
+
+def init_service_docs():
+    """Initializes Docs API Service"""
+    global SERVICE
+
+    creds = None
+    # The file token_docs.pickle stores the user's access and refresh tokens,
+    # and is created automatically when the authorization flow completes for the
+    # first time.
+    if os.path.exists(TOKEN_DOCS):
+        with open(TOKEN_DOCS, 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(CRED_DOCS, SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open(TOKEN_DOCS, 'wb') as token:
+            pickle.dump(creds, token)
+
+    SERVICE = build('docs', 'v1', credentials=creds)
+
+def init_service_drive():
+    """Initializes Drive API Service"""
+    global DRIVE_SERVICE
+
+    creds = None
+    # The file token_drive.pickle stores the user's access and refresh tokens,
+    # and is created automatically when the authorization flow completes for the
+    # first time.
+    if os.path.exists(TOKEN_DRIVE):
+        with open(TOKEN_DRIVE, 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(CRED_DRIVE, SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open(TOKEN_DRIVE, 'wb') as token:
+            pickle.dump(creds, token)
+
+    DRIVE_SERVICE = build('drive', 'v3', credentials=creds)
+
+# Initialize services
+def initialize():
+    init_service_docs()
+    init_service_drive()
+
+###########################
+### READING AND WRITING ###
+###########################
+
+# Parses one paragraph element and gets all text
+# Adapted from: https://developers.google.com/docs/api/samples/extract-text
+def read_paragraph_element(element):
+    """Returns the text in the given ParagraphElement.
+
+        Args:
+            element: a ParagraphElement from a Google Doc.
+    """
+    text_run = element.get('textRun')
+    if not text_run:
+        return ''
+    return text_run.get('content')
+
+# Parses document and reads num_bytes bytes starting at offset
+# Adapted from: https://developers.google.com/docs/api/samples/extract-text
+def read_strucutural_elements(elements, offset, num_bytes=None):
+    text = ''
+
+    for elem in elements:
+        if 'paragraph' in elem:
+            paras = elem.get('paragraph')
+            elems = paras.get('elements')
+
+            # There should only be one element per paragraph
+            assert(len(elems) == 1)
+            text += read_paragraph_element(elems[0])
+
+            # Each paragraph element adds a \n
+            text += '\n'
+    
+    # Decode from base64
+    decoded_str = decb64(text)
+
+    if num_bytes:
+        return decoded_str[offset : offset + num_bytes]
+    else:
+        return decoded_str
+
+# Args: doc id (string), offset (int), num_bytes (int/None)
+#
+# Reads num_bytes bytes of doc starting from offset and returns contents as a
+# string. (If num_bytes is None, reads the whole file by default)
+def read_doc(doc_id, offset, num_bytes=None):
+    global SERVICE
+
+    # Get document
+    document = SERVICE.documents().get(documentId=doc_id).execute()
+
+    # Read all elements
+    elements = document.get('body').get('content')
+
+    # The first element is always a null element which doesn't matter
+    elements = elements[1:]
+    contents = read_strucutural_elements(elements, offset, num_bytes)
+
+    # Get rid of the terminating \n if needed
+    if contents and contents[-1] == '\n':
+        return contents[:-1]
+
+    return contents
+
+# Args: doc id (string), offset (int), content to write (byte-string)
+#
+# Writes given content to specified doc at given offset
+def write_doc(doc_id, offset, content):
+    global SERVICE
+
+    # Since we store in base64, offset must be mapped too. Most straightforward
+    # way to do this is to read the doc, insert the string at the offset, then
+    # write it back in
+    current_contents = read_doc(doc_id, 0, None)
+
+    # Get the length of the base64 encoding of this string
+    b64_len = len(encb64(current_contents))
+
+    # print(content, current_contents)
+    content = str(content, 'utf-8')
+
+    # Insert the contents at offset in current contents
+    new_contents = current_contents[:offset] + content +\
+        current_contents[offset:]
+
+    # Clear all existing contents
+    delete = {
+        'deleteContentRange': {
+            'range': {
+                'startIndex': WRITE_OFFSET,
+                'endIndex': b64_len + 1
+            }
+        }
+    }
+
+    # Encode new contents in base64
+    b64_str = encb64(new_contents)
+
+    # Insert new contents
+    insert = {
+        'insertText': {
+            'location': {
+                'index': WRITE_OFFSET,
+            },
+            'text': b64_str
+        }
+    }
+
+    # Don't delete anything if the file is blank
+    requests = [ delete ] if b64_len > 0 else []
+    if b64_str: requests.append(insert)
+    
+    # If any deletion or insertion needs to be performed
+    if requests:
+        result = SERVICE.documents().batchUpdate(
+            documentId=doc_id, body={'requests': requests}).execute()
+
+#############################
+### CREATION AND DELETION ###
+#############################
+
+# Creates doc with given title and contents
+# Returns created document ID
+def create_doc(title, contents=None):
+    global SERVICE
+
+    # Construct document
+    doc = {
+        'title': title,
+    }
+
+    # Create the document
+    doc_obj = SERVICE.documents().create(body=doc).execute()
+    doc_id = doc_obj.get('documentId')
+
+    # Write to it if contents is not None
+    if contents:
+        write_doc(doc_id, 0, contents)
+
+    return doc_id
+
+# Deletes document with given document ID
+def delete_doc(doc_id):
+    global DRIVE_SERVICE
+
+    DRIVE_SERVICE.files().delete(fileId=doc_id).execute()
---
+++ gdocsfs.py
@@ -0,0 +1,416 @@
+#!/usr/bin/env python
+
+from __future__ import with_statement
+
+import os
+import sys
+import errno
+import gdoc
+
+from collections import defaultdict
+from fuse import FUSE, FuseOSError, Operations
+from stat import S_IFDIR, S_IFLNK, S_IFREG
+from time import time
+
+# Toggle debug mode
+DEBUG = False
+
+class GDocsFS(Operations):
+    def __init__(self, root):
+        self.root = root
+        self.files = {}
+
+        # Dict: file path -> file size
+        self.path_size = {}
+
+        # Dict: file path -> document ID (did)
+        self.path_dids = {}
+
+        # Dict: file descriptor -> document ID (did)
+        self.fd_dids = {}
+
+        # New stuff
+        self.data = {}
+        self.fd = 3
+        now = time()
+        self.files['/'] = {}
+
+        # Each file maps to an attribute dict and a subdirs dict
+        self.files['/']['subdirs'] = {}
+        self.files['/']['attr'] = dict(
+            st_mode=(S_IFDIR | 0o755),
+            st_ctime=now,
+            st_mtime=now,
+            st_atime=now,
+            st_nlink=2
+        )
+
+    # Helpers
+    # =======
+
+    def _full_path(self, partial):
+        if partial.startswith("/"):
+            partial = partial[1:]
+        path = os.path.join(self.root, partial)
+        return path
+
+    # Given a path to a file, traverses down and returns the dict associated
+    # with the given file
+    def _get_file_dict(self, path, files):
+        assert(path != '')
+        # if path == '':
+            # return files
+
+        if path == '/':
+            return files['/']
+        if path[0] == '/':
+            return self._get_file_dict(path[1:], files['/']['subdirs'])
+
+        folders = path.split('/')
+
+        if len(folders) == 1:
+            if folders[0] not in files:
+                raise FuseOSError(errno.ENOENT)
+            return files[folders[0]]
+
+        # Construct new path with top level dir removed
+        path = [x for x in folders if x != '']
+        new_path = '/'.join(path[1:])
+        curr_file = path[0]
+
+        if curr_file not in files:
+            raise FuseOSError(errno.ENOENT)
+        new_files = files[curr_file]['subdirs']
+        return self._get_file_dict(new_path, new_files)
+
+    # Updates nlink values of every directory along path by given amount
+    def _update_nlinks(self, path, files, delta):
+        # Should not be called on root
+        assert(path != '/')
+
+        if path == '':
+            return
+
+        folders = path.split('/')
+        if len(folders) == 1:
+            if folders[0] not in files:
+                raise FuseOSError(errno.ENOENT)
+            files[folders[0]]['attr']['st_nlink'] += delta
+            return
+
+        # Construct new path with top level dir removed
+        path = [x for x in path if x != '']
+        new_path = '/'.join(path[1:])
+        curr_file = path[0]
+
+        if curr_file not in files:
+            raise FuseOSError(errno.ENOENT)
+
+        # Update nlink
+        files[curr_file]['attr']['st_nlink'] += delta
+
+        # Recurse
+        return self._update_nlinks(new_path, files, delta)
+
+    # Generate dict for dir
+    def _gen_dict(self, mode):
+        d = {}
+        d['attr'] = dict(
+            st_mode=(S_IFDIR | mode),
+            st_nlink=2,
+            st_size=0,
+            st_ctime=time(),
+            st_mtime=time(),
+            st_atime=time(),
+        )
+        d['subdirs'] = {}
+        return d
+
+    # Recursive mkdir
+    # TODO: handle errno for dirs which already exist (don't throw err for calls
+    # to this function from create())
+    def _mkdir_helper(self, path, files, mode):
+        # Should not create root
+        # print('mkdir helper')
+        # print(path, files)
+        # return
+        # if path == '/dir1/dir2':return
+        if path == '/':
+            return files
+
+        if path == '':
+            return files
+
+        folders = path.split('/')
+        if len(folders) == 1:
+            if folders[0] not in files:
+                files[folders[0]] = self._gen_dict(mode)
+            else:
+                files[folders[0]]['attr']['st_nlink'] += 1
+            return files
+        # return files
+
+        # Construct new path with top level dir removed
+        path = [x for x in folders if x != '']
+        new_path = '/'.join(path[1:])
+        curr_file = path[0]
+
+        if curr_file not in files:
+            files[curr_file] = self._gen_dict(mode)
+        else:
+            # If the dir already exists, increase nlink
+            files[curr_file]['attr']['st_nlink'] += 1
+
+        new_files = files[curr_file]['subdirs']
+        added = self._mkdir_helper(new_path, new_files, mode)
+        files[curr_file]['subdirs'] = added
+        # print('Returning', files)
+        return files
+
+    # Filesystem methods
+    # ==================
+
+    def access(self, path, mode):
+        return
+        print('access', path)
+        full_path = self._full_path(path)
+        if not os.access(full_path, mode):
+            raise FuseOSError(errno.EACCES)
+        print('done access')
+
+    def chmod(self, path, mode):
+        print('chmod')
+        file_info = self._get_file_dict(path, self.files)
+        file_info['attr']['st_mode'] &= 0o770000
+        file_info['attr']['st_mode'] |= mode
+        return 0
+
+    def chown(self, path, uid, gid):
+        print('chown')
+        file_info = self._get_file_dict(path, self.files)
+        file_info['attr']['st_uid'] = uid
+        file_info['attr']['st_gid'] = gid
+
+    def getattr(self, path, fh=None):
+        # print('getattr', path)
+        # print(self.files)
+        file_info = self._get_file_dict(path, self.files)
+        # if path not in self.files:
+            # raise FuseOSError(errno.ENOENT)
+        return file_info['attr']
+
+    def readdir(self, path, fh):
+        print('readdir', path)
+        file_info = self._get_file_dict(path, self.files)
+        return ['.', '..'] + [x for x in file_info['subdirs']]
+
+    def readlink(self, path):
+        print('readlink')
+        return self.data[path]
+
+    def rmdir(self, path):
+        print('rmdir')
+        assert(path != '/')
+
+        # with multiple level support, need to raise ENOTEMPTY if contains any files
+        file_info = self._get_file_dict(path, self.files)
+        if len(file_info['subdirs'].keys()) > 0:
+            raise FuseOSError(errno.ENOTEMPTY)
+
+        # Get rid of the last file in the path
+        path_minus_one = '/' + '/'.join([x for x in path.split('/') if x != ''][:-1])
+        dir_to_delete = [x for x in path.split('/') if x != ''][-1]
+        assert(path_minus_one != '')
+        file_info = self._get_file_dict(path_minus_one, self.files)
+        file_info['subdirs'].pop(dir_to_delete)
+
+        # Update nlinks
+        self.files['/']['attr']['st_nlink'] -= 1
+        self._update_nlinks(path_minus_one[1:], self.files, -1)
+
+    def mkdir(self, path, mode):
+        print('mkdir', path)
+
+        # Create the dirs
+        self.files['/']['subdirs'] = self._mkdir_helper(path[1:], self.files['/']['subdirs'], mode)
+
+        # Update nlink of root
+        self.files['/']['attr']['st_nlink'] += 1
+        # print(self.files)
+
+    def statfs(self, path):
+        print('statfs')
+        return dict(f_bsize=512, f_blocks=4096, f_bavail=2048)
+
+    def unlink(self, path):
+        print('unlink', path)
+        if path in self.data:
+            self.data.pop(path)
+        # if path in self.files:
+        #     self.files.pop(path)
+
+        dirs = path.split('/')[1:]
+        files = self.files['/']['subdirs']
+        for i in dirs[:-1]:
+            if i != '':
+                files = files[i]['subdirs']
+        if dirs[-1] in files:
+            files.pop(dirs[-1])
+
+        if path in self.path_dids:
+            doc_id = self.path_dids[path]
+            gdoc.delete_doc(doc_id)
+
+    def symlink(self, target, source):
+        print('symlink', target, source)
+        self.mkdir(target, 0o777)
+        file_info = self._get_file_dict(target, self.files)
+        file_info['attr'] = dict(
+            st_mode=(S_IFLNK | 0o777),
+            st_nlink=1,
+            st_size=len(source)
+        )
+
+        self.data[target] = source
+
+    # TODO: implement rename with new updated heirarchical file structure
+    def rename(self, old, new):
+        print('rename', old, new)
+        if old in self.data:
+            self.data[new] = self.data.pop(old)
+        if old in self.files:
+            self.files[new] = self.files.pop(old)
+
+    def utimens(self, path, times=None):
+        print('utimens')
+        now = time()
+        atime, mtime = times if times else (now, now)
+
+        file_info = self._get_file_dict(path, self.files)
+        file_info['attr']['st_atime'] = atime
+        file_info['attr']['st_mtime'] = mtime
+
+    # File methods
+    # ============
+
+    def open(self, path, flags):
+        print('open', path, oct(flags))
+        self.fd += 1
+
+        doc_id = self.path_dids[path]
+        self.fd_dids[self.fd] = doc_id
+        return self.fd
+
+    def create(self, path, mode):
+        print('create', path)
+        assert(path != '/')
+        # Create a new document
+        doc_id = gdoc.create_doc(path)
+
+        # Map path -> file size
+        self.path_size[path] = 0
+
+        # Map path -> document ID
+        self.path_dids[path] = doc_id
+
+        # Get path to file
+        folders = path.split('/')
+        path_minus_one = '/' + '/'.join([x for x in folders if x != ''][:-1])
+        file_to_create = [x for x in folders if x != ''][-1]
+
+        # Create dir till file, add files attributes
+        self.files['/']['subdirs'] = self._mkdir_helper(path_minus_one[1:], self.files['/']['subdirs'], mode)
+        assert(path_minus_one != '')
+        file_info = self._get_file_dict(path_minus_one, self.files)
+
+        new_file = {}
+        new_file['attr'] = dict(
+            st_mode=(S_IFREG | mode),
+            st_nlink=1,
+            st_size=0,
+            st_ctime=time(),
+            st_mtime=time(),
+            st_atime=time()
+        )
+
+        file_info['subdirs'][file_to_create] = new_file
+        print(self.files)
+
+        self.fd += 1
+        self.fd_dids[self.fd] = doc_id
+        return self.fd
+
+    def read(self, path, length, offset, fh):
+        print('read')
+
+        if fh not in self.fd_dids:
+            return FuseOSError(errno.EBADF)
+
+        # Get the doc ID
+        doc_id = self.fd_dids[fh]
+        string = gdoc.read_doc(doc_id, offset, length)
+
+        # Possibly add \n ?
+        s = ('%s' % string)
+        return s.encode('utf-8')
+
+    def write(self, path, buf, offset, fh):
+        print('write')
+
+        if fh not in self.fd_dids:
+            return FuseOSError(errno.EBADF)
+
+        # Get the doc ID
+        doc_id = self.fd_dids[fh]
+        gdoc.write_doc(doc_id, offset, buf)
+
+        # Update the file size
+        self.path_size[path] += len(buf)
+        assert(path != '')
+        file_info = self._get_file_dict(path, self.files)
+        file_info['attr']['st_size'] += len(buf)
+
+        return len(buf)
+
+    def truncate(self, path, length, fh=None):
+        print('truncate')
+
+        # Get the current content
+        doc_id = self.path_dids[path]
+        curr_content = gdoc.read_doc(doc_id, 0, None)
+
+        # Make sure extending the file fills it in with zero bytes
+        new_content = curr_content.ljust(length, '\x00').encode('utf-8')
+        # Write the new truncated content
+        gdoc.write_doc(doc_id, 0, new_content)
+
+        # Update the file size
+        self.path_size[path] = length
+        assert(path != '')
+        file_info = self._get_file_dict(path, self.files)
+        file_info['attr']['st_size'] = length
+
+    # No buffers so no need for this function
+    def flush(self, path, fh):
+        print('flush')
+        return
+
+    def release(self, path, fh):
+        print('release')
+        # Remove fd -> doc ID mapping and close fd
+        self.fd_dids[fh] = None
+
+    def fsync(self, path, fdatasync, fh):
+        print('fsync')
+        return self.flush(path, fh)
+
+
+def main(mountpoint):
+    # Initialize gDocs services
+    gdoc.initialize()
+
+    # Run filesystem
+    FUSE(GDocsFS(mountpoint), mountpoint, nothreads=True, foreground=True, debug=DEBUG)
+
+if __name__ == '__main__':
+    main(sys.argv[1])
